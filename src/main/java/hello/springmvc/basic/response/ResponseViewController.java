ce2a0274abc3e483a572a8bef39e01a498e75c58
hyejin0916 <rkdgpwlsz@naver.com>
2025-12-15 22:04:52 +0900

    feat: section7-55 render response views using ModelAndView, String, and void

---
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,32 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "hello!");
+
+        return "response/hello";
+    }
+
+    @RequestMapping("/response/hello")
+    // 컨트롤러의 경로와 뷰의 논리적 이름이 같을 때, void인 경우 논리적 뷰의 이름으로 진행됨
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "hello!");
+    }
+}
+
